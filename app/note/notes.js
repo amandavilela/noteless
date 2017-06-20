51acc5b7b3cf2459f7199d39b4ef75d0255cad2f
henrique-htg <henrique.develop@hotmail.com>
2017-06-20 19:23:16 -0300

    Include CRUD control to notes with Firebase Database Realtime.

--- app/note/notes.js
+++ app/note/notes.js
@@ -6,10 +6,10 @@
 
   function NotesController($stateParams, $firebaseArray) {
 
-	vm = this;
+    vm = this;
     var database = firebase.database().ref('notes');
     vm.notes = $firebaseArray(database);
-  
+
     /*//Funcoes padrão do Firebase para escutar mudanças e inclusões de um Child.
     database.on('child_added', e => {
       vm.notes.push(e.val());
@@ -20,20 +20,29 @@
     });*/
 
     function getNote(id) {
-      var note = vm.notes.map(function(x) { if (x.id == id) return x; else return ""});
+      var note = vm.notes.map(function (x) { if (x.id == id) return x; else return "" });
       return note;
     }
 
-    (function isEditing () {
+    (function isEditing() {
       $stateParams.id ? vm.editing = true : vm.editing = false;
-	  if (vm.notes.length > 0) {
-		vm.note = angular.copy(getNote($stateParams.id));
-		console.log(note);
-      }	  
+      if (vm.notes.length > 0) {
+        vm.note = angular.copy(getNote($stateParams.id));
+        console.log(note);
+      }
     })();
 
     vm.add = add;
-    function add(note) {
+    function add(note) {    
+      var newKey = database.push().key;
+
+      note = {"id":newKey,"subject":note.subject,"body":note.body, "postDate":Date()};
+      
+      var updates = {};
+      updates[newKey] = note;
+
+      database.update(updates);
+
       vm.notes.push(angular.copy(note));
       console.log(vm.notes);
     }
@@ -42,6 +51,14 @@
     function remove(note) {
       var removeNote = vm.notes.indexOf(note);
       if (removeNote > -1) {
+        database.child(vm.notes[removeNote].id).remove().then(function () {
+                // File deleted successfully
+                console.log("note has removed");
+            }).catch(function (error) {
+                // An error occurred!
+                console.log(error.code + " - " + error.message);
+            });        
+        
         vm.notes.splice(removeNote, 1);
       }
       console.log(vm.notes);
